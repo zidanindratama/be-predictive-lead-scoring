fabe9f64f7588d77b3b82f5947f5f438f63f0b99
zidanindratama <zidanindratama31@gmail.com>
2025-11-04 13:38:36 +0700

    fix: downgrade prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,27 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
+import {
+  INestApplication,
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
   }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
   async enableShutdownHooks(app: INestApplication) {
-    this.$on('error', async () => {
+    // @ts-ignore
+    this.$on('beforeExit', async () => {
       await app.close();
     });
   }
